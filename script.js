9d6a034563b62d15748981113ecae677cc22a4c8
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-06-25 13:27:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 // complete this js code
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-}
+// function Person(name, age) {}
 
-Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-};
+// function Employee(name, age, jobTitle) {}
+
+// // Do not change code below this line
+// window.Person = Person;
+// window.Employee = Employee;
+
+// Person constructor function
+// complete this js code
+function Person(name, age) {
+	this._name=name;
+	this._age=age;
+}
+Person.prototype.greet=function(){
+	console.log("Hello, my name is "+this._name+" and I am "+this._age+" years old.")
+}
 
 function Employee(name, age, jobTitle) {
-  Person.call(this, name, age);
-  this.jobTitle = jobTitle;
+	Person.call(this,name,age)
+	this.jobTitle= jobTitle
 }
 
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
+Employee.prototype = Object.create(Person.prototype)
 
-Employee.prototype.jobGreet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-};
+Employee.prototype.constructor= Employee
+Employee.prototype.jobGreet = function (){
+	console.log("Hello, my name is "+this._name+", I am "+this._age+" years old, and my job title is "+this.jobTitle+".")
+}
+
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
